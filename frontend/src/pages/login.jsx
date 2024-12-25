dc08c9bd951e25717fd29f3c88ade4f28d6e2827
Sarthak-0106 <vsarthak1234@gmail.com>
2024-12-25 23:00:14 +0530

    Complete

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,41 +1,71 @@
-import React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import logo from "../images/logo.png"
+import { Link, useNavigate } from 'react-router-dom';
+import image from "../images/authPageSide.png";
+import { api_base_url } from '../helper';
 
-const login = () => {
-    const [Email, setEmail] = useState('');
-    const [Password, setPassword] = useState('');
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
-    const submitForm = (e) => {
-        e.preventDefault();
-    }
+  const [error, setError] = useState("");
 
-    return (
-        <>
-            <div className="container w-screen min-h-screen flex items-center justify-between px-[100px]">
-                <div className="left w-[40%]">
-                    <img src="  " alt="" />
-                    <form onSubmit={submitForm} className='w-full mt-[30px]' action="">
+  const navigate = useNavigate();
 
-                        <div className="inputbox">
-                            <input onChange={(e)=>{setEmail(e.target.value)}} value={Email} type="email" placeholder="Email" />
-                        </div>
-                        
-                        <div className="inputbox">
-                            <input onChange={(e)=>{setPassword(e.target.value)}} value={Password} type="password" placeholder="Password" />
-                        </div>
+  const submitForm = (e) => {
+    e.preventDefault();
+    fetch(api_base_url + "/login",{
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: pwd
+      })
+    }).then(res => res.json()).then(data => {
+      if(data.success === true){
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("userId", data.userId);
+        setTimeout(() => {
+          window.location.href = "/"
+        }, 200);
+      } else {
+        setError(data.message);
+      }
+    })
+  }
 
-                        <p className="text-[gray] mt-[5px]">Don't have an account? <Link to="/signUp" className='text-[#00AEEF]'>SignUp</Link></p>
-                        
-                        <button className='btnblue w-full mt-[20px]'>Login</button>
-                    </form>
-                </div>
-                <div className="right">
-                    
-                </div>
+  return (
+    <>
+      <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
+        <div className="left w-[35%]">
+          <img className='w-[200px]' src={logo} alt="" />
+          <form onSubmit={submitForm} className='w-full mt-[60px]' action="">
+
+            <div className="inputBox">
+              <input required onChange={(e)=>{setEmail(e.target.value)}} value={email} type="email" placeholder='Email'/>
             </div>
-        </>
-    )
+
+            <div className="inputBox">
+              <input required onChange={(e)=>{setPwd(e.target.value)}} value={pwd} type="password" placeholder='Password'/>
+            </div>
+
+            <p className='text-[gray]'>Don't have an account <Link to="/signUp" className='text-[#00AEEF]'>Sign Up</Link></p>
+            
+            <p className='text-red-500 text-[14px] my-2'>{error}</p>
+
+            <button className="btnBlue w-full mt-[20px]">Login</button>
+          </form>
+        </div>
+        <div className="right w-[55%]">
+          <img className='h-[100vh] w-[100%] object-cover' src={image} alt="" />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default login;
+export default Login
